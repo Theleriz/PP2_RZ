afa0a26ef08e315fb1ccf33c36f1e841aa124ae8
Theleriz <zanatrisat54@gmail.com>
2025-01-29 02:40:31 +0500

    class Vector in procces

--- Lab3/ClassExample.py
+++ Lab3/ClassExample.py
@@ -1,45 +1,6 @@
-import math
+from MyVector import Vector
 
+v1 = Vector(1, 1)
+v2 = Vector(2, 2)
 
-class MyPointClass:
-    def __init__(self, x: float = 0, y: float = 0):
-        self.x = x
-        self.y = y
-    
-
-    def __repr__(self):
-        return f"x: {self.x} y: {self.y}"
-    
-
-    def __add__(self, other):
-        if isinstance(other, MyPointClass):
-            return MyPointClass(self.x + other.x, self.y + other.y)
-        else:
-            return NotImplemented
-    
-    
-    def __sub__(self, other):
-        if isinstance(other, MyPointClass):
-            return MyPointClass(self.x - other.x, self.y - other.y)
-        else:
-            return NotImplemented
-
-    def __eq__(self, other):
-        if isinstance(other, MyPointClass):
-            if((self.x == other.y) and (self.y == other.y)):
-                return True
-            else:
-                return False
-        else:
-            return NotImplemented
-
-    def lenght(self) -> float:
-        return math.sqrt(math.pow(self.x, 2) + math.pow(self.y, 2)) 
-
-
-
-fr = MyPointClass(1, 1)
-sc = MyPointClass(2, 3)
-
-print(fr == sc)
-
+print(v1 * v2)
---
+++ Lab3/MyVector.py
@@ -0,0 +1,68 @@
+import math
+
+
+class Vector:
+    def __init__(self, x: float = 0, y: float = 0):
+        self.x = x
+        self.y = y
+    
+    def __repr__(self):
+        return f"x: {self.x} y: {self.y}"
+    
+    def __add__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y)
+        else:
+            return NotImplemented
+        
+    def __sub__(self, other):
+        if isinstance(other, Vector):
+            return Vector(self.x - other.x, self.y - other.y)
+        else:
+            return NotImplemented
+
+    def __eq__(self, other):
+        if isinstance(other, Vector):
+            if((self.x == other.y) and (self.y == other.y)):
+                return True
+            else:
+                return False
+        else:
+            return NotImplemented
+
+    def __mul__(self, other):
+        if isinstance(other, Vector):
+                return (self.x * other.x) + (self.y * other.y)
+        else:
+            return NotImplemented
+
+    def lenght(self) -> float:
+        return math.sqrt(math.pow(self.x, 2) + math.pow(self.y, 2)) 
+
+    def unitForm(self):
+        return Vector((self.x / self.lenght()), (self.y / self.lenght()))
+    
+    def sign(self, other):
+        if isinstance(other, Vector):
+            return (self.x * other.y) - (self.y * other.x)
+        else:
+            return NotImplemented
+        
+    def angle(self, other):
+        if isinstance(other, Vector):
+            cosAngle = (self * other) / (math.sqrt(self.lenght() * other.lenght()))
+            return self.sign(self, other) * math.acos(cosAngle)
+        else:
+            return NotImplemented
+
+EX = Vector(1, 0)
+EY = Vector(0, 1)    
+
+
+
+    
+
+
+
+
+

6b651f27db6f83de4a58d34f97b6ff516fc68f2d
Theleriz <zanatrisat54@gmail.com>
2025-02-19 00:01:14 +0500

    pass

--- Lab4/json/task.py
+++ Lab4/json/task.py
@@ -1,15 +1,18 @@
-import json as js
+import json
 
-data = js.load("sample-data.json", 'r')
+with open('data.json', 'r') as file:
+    data = json.load(file)
 
-
-print("Interface Status")
-print("=" * 80)
-print(f"{'DN':<30} {'Description':<20} {'Speed':<7} {'MTU':<5}")
+imdata = data["imdata"]
+headers = ["dn", "descr", "speed", "mtu"]
+print("=" * 90)
+print(f"{headers[0]:<50} {headers[1]:<20} {headers[2]:<10} {headers[3]:<10}")
 print("-" * 80)
-
-interface = data['imdata']
-print(interface)
-
-for i in interface:
-    dn = interface.get("dn", "none")
+for item in imdata:
+    attributes = item['l1PhysIf']['attributes']
+    dn = attributes.get("dn")
+    descr = attributes.get("descr")
+    speed = attributes.get("speed") 
+    mtu = attributes.get("mtu")
+ 
+    print(f"{dn:<50} {descr:<20} {speed:<10} {mtu:<10}")

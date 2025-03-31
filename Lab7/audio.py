db8546250b59946f154371215a9e8e15ae6ad7e7
Theleriz <zanatrisat54@gmail.com>
2025-03-31 09:07:20 +0500

    add new coin  image, debug audio in lab7

--- Lab7/audio.py
+++ Lab7/audio.py
@@ -1,60 +1,55 @@
 import pygame
 import os
 
-
 pygame.init()
 pygame.mixer.init()
 
 screen = pygame.display.set_mode((400, 300))
 folder = 'folder'
+
 music_list = [song for song in os.listdir(folder) if song.endswith('.mp3')]
+
 index = 0
 runtime = True
 is_playing = True
 
-
 song = os.path.join(folder, music_list[index])
 pygame.mixer.music.load(song)
 pygame.mixer.music.play()
-    
+
 while runtime:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             runtime = False
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
+                if is_playing:
+                    pygame.mixer.music.pause()
+                    is_playing = False
+                else:
+                    pygame.mixer.music.unpause()
+                    is_playing = True
 
-        #exchange to keydown
-    keys = pygame.key.get_pressed()
+            elif event.key == pygame.K_RIGHT:
+                pygame.mixer.music.stop()
+                index = (index + 1) % len(music_list)
+                song = os.path.join(folder, music_list[index])
+                pygame.mixer.music.load(song)
+                pygame.mixer.music.play()
+                is_playing = True
 
-    if keys[pygame.K_ESCAPE]:
-        break
-
-    if keys[pygame.K_SPACE]:
-        if is_playing:
-            pygame.mixer.music.stop()
-            is_playing = False
-        else:
-            pygame.mixer.music.play()
-            
-            
-
-    elif keys[pygame.K_RIGHT]:
-        pygame.mixer.music.stop()
-        index += 1 % len(music_list)
-        song = os.path.join(folder, music_list[index % len(music_list)])
-        pygame.mixer.music.load(song)
-        pygame.mixer.music.play()
-
-
-    elif keys[pygame.K_LEFT]:
-        pygame.mixer.music.stop()
-        index -= 1 % len(music_list)
-        song = os.path.join(folder, music_list[index % len(music_list)])
-        pygame.mixer.music.load(song)
-        pygame.mixer.music.play()
+            elif event.key == pygame.K_LEFT:
+                pygame.mixer.music.stop()
+                index = (index - 1) % len(music_list)
+                song = os.path.join(folder, music_list[index])
+                pygame.mixer.music.load(song)
+                pygame.mixer.music.play()
+                is_playing = True
 
     pygame.display.flip()
     pygame.time.Clock().tick(60)
+
+#разобраться с нажатиям несколько раз сразу
 
 pygame.quit()
--- Lab8/task1/MainGameStarter.py
+++ Lab8/task1/MainGameStarter.py
@@ -102,7 +102,7 @@ class Player(pygame.sprite.Sprite):
 class BuffCoin(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("BuffCoin.png")
+        self.image = pygame.image.load("BuffCoin2.png")
         self.rect = self.image.get_rect()
         self.rect.center = (random.randint(40,SCREEN_WIDTH-40), 0)
         
